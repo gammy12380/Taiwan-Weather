dce150c9be84fe091a6722535ce9525150e6ad06
gammy12380 <gammy12380@gmail.com>
2023-08-22 22:17:20 +0800

    ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr:true,
+  ssr:false,
   css: ['~/assets/css/tailwind.css'],
   modules:[
     '@nuxtjs/tailwindcss',
